d046c047f972738b6cf0ec183b9e539cf9103b40
Passing-by-github <130006613+Passing-by-github@users.noreply.github.com>
2023-11-28 12:27:23 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,21 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ -e "student-submission/ListExamples.java" ];
+then 
+echo "File does exists"
+else
+echo "file doesn't exist"
+exit
+fi 
+
+cp -r student-submission/*.java > grading-area
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore ArrayTests grading-area/*.java
+
+grep "Tets run:" > grading-area/test_results.txt
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
