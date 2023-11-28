0169f4efcdcd67bf70bb15db5d49f432dec6f44c
Passing-by-github <130006613+Passing-by-github@users.noreply.github.com>
2023-11-28 13:17:55 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,23 +5,35 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission > grading-area/clone-message.txt 2>&1
 echo 'Finished cloning'
 
-if [ -e "student-submission/ListExamples.java" ];
+if [[ -f "student-submission/ListExamples.java" ]]
 then 
-echo "File does exists"
-else
-echo "file doesn't exist"
+echo "file exists!"
+else 
+echo "file doesn't exists"
 exit
 fi 
 
-cp -r student-submission/*.java > grading-area
+cp -r student-submission/*   grading-area
+cp ./*.java grading-area
 
-javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore ArrayTests grading-area/*.java
 
-grep "Tets run:" > grading-area/test_results.txt
+
+javac -cp $CPATH *.java grading-area/*.java 2> grading-area/compile-error.txt
+
+
+
+if [[ $? -ne 0 ]]; 
+then 
+    echo "compile error"
+    exit
+else
+    echo "Compile successful" 
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore grading-area/TestListExamples > grading-area/output.txt 
 
 
 # Draw a picture/take notes on the directory structure that's set up after
